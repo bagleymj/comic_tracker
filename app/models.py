3fe8fdd764e72474cc8c414a147bff0b15a72c21
Mitchell Bagley <mitchell.bagley@earnestassoc.com>
2024-11-26 23:06:14 -0500

    Add Books Table

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+book_issue_association = Table(
+    "book_issue",
+    Base.metadata,
+    Column("book_id", Integer, ForeignKey("books.id"), primary_key=True),
+    Column("issue_id", Integer, ForeignKey("issues.id"), primary_key=True) 
+)
 class Series(Base):
     __tablename__ = "series"
     id = Column(Integer, primary_key=True, index=True)
@@ -17,3 +23,10 @@ class Issue(Base):
     title = Column(String, nullable=False)
     series_id = Column(Integer, ForeignKey("series.id"), nullable=False)
     series = relationship("Series",back_populates="issues")
+    books = relationship("Book", secondary=book_issue_association, back_populates="issues")
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    issues = relationship("Issue", secondary=book_issue_association, back_populates="books")
